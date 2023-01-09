425a06df3d6319a26d40951cb5a520c7c34e9bdf
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2023-01-09 15:48:22 +0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -6,3 +6,15 @@ def send_telegram_message(text):
     url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text
     results = requests.get(url_req)
     return results.json()
+
+
+def main(request):
+    if request.method == 'POST':
+        try:
+            pass
+        except Exception as _ex:
+            send_telegram_message(u'\U0001F525' + u'\U0001F525' + u'\U0001F525' + f'''
+            Error occurred \n
+            Error text: {_ex} \n
+            Request: {request.POST}
+            ''')
